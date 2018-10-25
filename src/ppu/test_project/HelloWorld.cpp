b1fefdfeeb8d7e2396a36fd0a3aec4015b8fb9e4
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2018-10-25 16:20:53 +0200

    Add Jenkinsfile (#2973)
    Change-Id: I2592f285454e1cf20e54ccef8c6d9bb7407afc8f

--- src/ppu/test_project/HelloWorld.cpp
+++ src/ppu/test_project/HelloWorld.cpp
@@ -14,6 +14,6 @@ extern "C" {
 void
 start(void)
 {
-	vector uint8_t lhs = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
+	vector uint8_t lhs __attribute__((unused)) = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
 }
 }
